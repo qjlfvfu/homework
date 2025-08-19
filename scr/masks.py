9c4817a4c3254b0078d88396da19ab67def44c99
Nikolai <sir.nikolai00@mail.ru>
2025-08-19 19:06:25 +0300

    основные файлы программы

--- scr/masks.py
+++ scr/masks.py
@@ -1,14 +1,13 @@
-def get_mask_card_number(card_number:int)-> int :
+def get_mask_card_number(card_number: int) -> int:
     """Функция которая возвращает строку после ввода  номера карты"""
     clean_card_number = card_number.replace(" ", "")
     if len(clean_card_number) != 16:
         return "Неверный ввод номера карты"
-
     masked_number = f"{clean_card_number[:4]} {clean_card_number[4:6]}** **** {clean_card_number[-4:]}"
     return masked_number
 
 
-def get_mask_account(account_user:int) -> int:
+def get_mask_account(account_user: int) -> int:
     """Функция которая возвращает строку после ввода аккаунта"""
     masked_acc = f"**{account_user[:-4]}"
     return masked_acc
--- scr/widget.py
+++ scr/widget.py
@@ -1,19 +1,20 @@
-from typing import Union
-from masks import get_mask_account
 from datetime import datetime
+from typing import Union
+
+from masks import get_mask_account, get_mask_card_number
+
+
 def get_date(date_str):
-     ''' Функция превращает вводимую дату в форматированую '''
-     dt = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f")
-     # Форматируем datetime в строку с нужным форматом
-     return dt.strftime("%d.%m.%Y")
+    """Функция превращает вводимую дату в форматированую"""
+    dt = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f")
+    # Форматируем datetime в строку с нужным форматом
+    return dt.strftime("%d.%m.%Y")
 
 
-def account(account_info:Union[str,int] ) -> str :
-    '''Функция считывает информацию о счете\карте маскируя их по разному'''
-    if  "счет" in account_info.lower():
-        account_number = account_info.split()[-1]
-        masked_account = f"**{account_number[:-4]}"
-        return f"Счет {masked_account}"
+def mask_account_card(account_info: Union[str, int]) -> str:
+    """Функция считывает информацию о счете\карте маскируя их по разному"""
+    if "счет" in account_info.lower() or account_info.lower().startswith("счет"):
+        # Используем функцию для маскировки счета
+        return get_mask_account(account_info)
     else:
-        masked_number = f"{account_info[:4]} {account_info[4:6]}** **** {account_info[-4:]}"
-        return masked_number
+        return get_mask_card_number
