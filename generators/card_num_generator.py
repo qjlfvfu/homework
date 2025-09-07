4619409966d142e27755d34aceddd221f03aaee6
Nikolai <sir.nikolai00@mail.ru>
2025-09-07 19:01:07 +0300

    вставил аргументы в генератор карт

--- generators/card_num_generator.py
+++ generators/card_num_generator.py
@@ -1,11 +1,11 @@
 import random
 
 
-def card_number_generator():
+def card_number_generator(start=1,stop=9999999999999999):
     """Генератор номеров банковских карт"""
     while True:
         # Генерируем случайное число
-        random_digit = random.randint(1, 9999999999999999)
+        random_digit = random.randint(start, stop)
         digit_str = str(random_digit).zfill(16)
         # Форматируем номер карты
         card_number = f"{digit_str[:4]} {digit_str[4:8]} {digit_str[8:12]} {digit_str[12:16]}"
