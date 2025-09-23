d6b918412e2b501f08817a2c519d202e4a2c7fa2
Nikolai <sir.nikolai00@mail.ru>
2025-09-23 20:02:31 +0300

    всандалил ЛОГи проверил отдельно логгирование и оно работает, логгирование в функциях не проверял

--- src/external_api.py
+++ src/external_api.py
@@ -1,12 +1,10 @@
 import os
 from typing import Any, Optional
-
 import requests
-from dotenv import load_dotenv
 
-# Загрузка API ключа
-load_dotenv(".env")
+
 API_KEY = os.getenv("API_KEY")
+
 
 
 def convert_currency(from_curr: str, to_currs: Any, date: Optional[str] = None) -> Any:
@@ -17,18 +15,15 @@ def convert_currency(from_curr: str, to_currs: Any, date: Optional[str] = None) 
     :param to_currs: в валюты вводимую пользователем (USD, EUR)
     :param date: дата (опционально)
     """
-    url = "https://api.apilayer.com/currency_data/convert"
-    headers = {"apikey": API_KEY}
-    try:
-        amount = float(input("Введите сумму которую хотите конвертировать в Рублях"))
-    except Exception as e:
-        print(f"❌ Произошла ошибка: {e}")
-        return None
 
-    params = {"amount": amount, "from": from_curr, "to": to_currs}
+    url = f"https://api.apilayer.com/exchangerates_data/convert?to={from_curr}&from={to_currs}&amount=1"
 
-    if date:
-        params["date"] = date
+    payload = {}
+    headers = {
+        "apikey": API_KEY
+    }
 
-    response = requests.get(url, headers=headers, params=params)
-    return response.json() if response.status_code == 200 else None
+    response = requests.request("GET", url, headers=headers, data=payload)
+    status_code = response.status_code
+    result = response.text
+    return result if status_code == 200 else status_code
