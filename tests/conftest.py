599ea3d57e54f40383ad58160c69e07caecca46b
Nikolai <sir.nikolai00@mail.ru>
2025-10-17 19:21:53 +0300

    сделал чтение файла как тест

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,6 @@ import pytest
 
 from src.classification import Category, Product
 
-
 # Фикстуры для тестов
 
 
@@ -85,11 +84,9 @@ def mock_requests():
 @pytest.fixture
 def list_category():
     """Фикстура возвращает Список продуктов в категории"""
-    return Category(
-        'Мясо' ,[" Тофик",
-        "Бобик",
-        "Вася"], "Грустно но вкусно")
+    return Category("Мясо", [" Тофик", "Бобик", "Вася"], "Грустно но вкусно")
+
 
 @pytest.fixture
 def sausage():
-    return Product('Колбаса','Краковская колбаса прямо из под собаки',25,1.40)
+    return Product("Колбаса", "Краковская колбаса прямо из под собаки", 25, 1.40)
--- tests/test_classification.py
+++ tests/test_classification.py
@@ -1,18 +1,18 @@
 import json
-from typing import List, Any
+from typing import Any, List
 
 from src.classification import Category
 
 
 def test_init(sausage):
-    assert sausage.name=='Колбаса'
-    assert sausage.description == 'Краковская колбаса прямо из под собаки'
+    assert sausage.name == "Колбаса"
+    assert sausage.description == "Краковская колбаса прямо из под собаки"
     assert sausage.quantity == 25
     assert sausage.price == 1.40
 
 
 def test_category_init(list_category):
     assert list_category.total_products == 3
-    assert list_category.name =="Мясо"
+    assert list_category.name == "Мясо"
     assert list_category.description == "Грустно но вкусно"
     assert list_category.total_categories == 1
