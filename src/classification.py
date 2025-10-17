90aa223597073f5d0931aff82ab8c42ebc434337
Nikolai <sir.nikolai00@mail.ru>
2025-10-17 19:21:49 +0300

    сделал чтение файла как тест

--- src/classification.py
+++ src/classification.py
@@ -6,47 +6,43 @@ from src.utils import load_transactions
 class Product:
     name = str
     description = str
-    price=float
-    quantity=int
+    price = float
+    quantity = int
     """Класс описания свойств продукта"""
-    def __init__(self,name,description,quantity,price):
-        self.name=name
-        self.quantity=quantity
-        self.description=description
-        self.price=price
+
+    def __init__(self, name, description, quantity, price):
+        self.name = name
+        self.quantity = quantity
+        self.description = description
+        self.price = price
 
 
 class Category:
     """Класс категорий продукта"""
+
     products = []
-    total_categories=0
-    total_products=0
-    def __init__(self,name,products,description):
-        self.name=name
-        self.products=products
-        self.description=description
-            # Автоматически обновляем атрибуты класса при создании объекта
+    total_categories = 0
+    total_products = 0
+
+    def __init__(self, name, products, description):
+        self.name = name
+        self.products = products
+        self.description = description
+        # Автоматически обновляем атрибуты класса при создании объекта
         Category.total_categories += 1
         Category.total_products += len(products)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     result = load_transactions("products.json")
     products = []
     for item in result:
         product = Product(
-            name=item['name'],
-            description=item['description'],
-            quantity=item['quantity'],
-             price=item['price']
-                )
+            name=item["name"], description=item["description"], quantity=item["quantity"], price=item["price"]
+        )
         products.append(product)
         print(products)
         categories = []
         for category in result:
-            product = Category(
-                 name=item['name'],
-                description=item['description'],
-                 products=item['products']
-            )
+            product = Category(name=item["name"], description=item["description"], products=item["products"])
             print(categories)
